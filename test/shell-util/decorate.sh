107e7b96b1f34b8e0461b3f411e2d6d1139f6d8b
Martin Salazard <martin.der.aze@gmail.com>
2017-02-09 00:06:32 +0100

    shell-util : disable decoration tests [VIP]

--- test/shell-util/decorate.sh
+++ test/shell-util/decorate.sh
@@ -17,6 +17,15 @@ testSetThenGetTextDecoration() {
 	local foobar_decoration=$(mdu_getTextDecoration foobar)
 	assertEquals blue_and_shiny "$foobar_decoration"
 }
+testSetThenGetThenRemoveTextDecoration() {
+	local foobar_decoration
+	mdu_setTextDecoration foobar2 blue_and_transparent
+	foobar_decoration=$(mdu_getTextDecoration foobar2)
+	assertEquals blue_and_transparent "$foobar_decoration"
+	mdu_unsetTextDecoration foobar2
+	foobar_decoration=$(mdu_getTextDecoration foobar2)
+	assertEquals "" "$foobar_decoration"
+}
 
 #testSingleDecoration() {
 #	mdu_setTextDecoration quote __said_words__
@@ -24,6 +33,19 @@ testSetThenGetTextDecoration() {
 #	local decorated="$(decorate "$text" "__-__")"
 #	assertEquals "Platon said __said_words__something useful__-__." "$decorated"
 #}
+
+#testNestedWithSibblingDecoration() {
+#	mdu_setTextDecoration bold __bold__
+#	mdu_setTextDecoration red __red__
+#	mdu_setTextDecoration green __green__
+#	mdu_setTextDecoration yellow __yellow__
+#	mdu_setTextDecoration blue __blue__
+#	mdu_setTextDecoration underline __underline__
+#	local text="Le voisin {yellow{a dit quelque chose que l'{underline{on {bold{peut}} traduire}} par \"{red{c'est de la faute à {blue{Rousseau}} et {green{Voltaire}}... rien de plus}}\"}}. Mais il était saoul."
+#	local decorated="$(decorate "$text" "__-__")"
+#	assertEquals "Le voisin a dir__-__." "$decorated"
+#}
+
 
 
 . "$test_root_dir/shunit2-2.0.3/src/shell/shunit2"
