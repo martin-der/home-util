6afd6d54a24bef93846c936b27e6d3a494558fed
Martin Salazard <martin.der.aze@gmail.com>
2017-02-11 23:39:33 +0100

    move tests

---
+++ test/config-util/read.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#test_root_dir="$HOME/dev/prog/home-util/test"
+pushd "$(dirname "$0")" > /dev/null
+root_dir="$(pwd -P)/../.."
+popd > /dev/null
+test_root_dir="${root_dir}/test"
+
+oneTimeSetUp() {
+	. "${root_dir}/config-util.sh" || exit 1
+	RESOURCES_DIR="${test_root_dir}/resources"
+}
+
+
+setUp() {
+	:
+}
+tearDown() {
+	:
+}
+
+
+testExportProperty() {
+
+	local properties_2_var="foooo=FOO
+passwd=PASSWORD
+   baaar  =BAR
+fiiibuuur.status=STATE"
+
+	convertConfigKeyAndExportToEnvVariableIfExists "foooo" "enemy" "$properties_2_var"
+	assertEquals "Var name for key 'foooo' has been found" 0 $?
+	assertEquals "Var 'FOO' has been created with correct value" "$FOO" "enemy" 
+
+	convertConfigKeyAndExportToEnvVariableIfExists "passwd" "12345" "$properties_2_var"
+	assertEquals "Var name for key 'passwd' has been found" 0 $?
+	assertEquals "Var 'PASSWORD' has been created with correct value" "$PASSWORD" "12345" 
+
+	convertConfigKeyAndExportToEnvVariableIfExists "baaar" "Is this a bar?" "$properties_2_var"
+	assertEquals "Var name for key 'baaar' has been found" 0 $?
+	assertEquals "Var 'BAR' has been created with correct value" "$BAR" "Is this a bar?" 
+
+	convertConfigKeyAndExportToEnvVariableIfExists "fiiibuuur.status" "down" "$properties_2_var"
+	assertEquals "Var name for key 'fiiibuuur.status' has been found" 0 $?
+	assertEquals "Var 'STATE' has been created with correct value" "$STATE" "down" 
+
+}
+testExportProperty() {
+	local properties_2_var="foooo=FOO
+passwd=PASSWORD
+   baaar  =BAR
+fiiibuuur.status=STATE"
+
+	convertConfigKeyAndExportToEnvVariableIfExists "plain.test.password" "23456" "$properties_2_var"
+	assertNotSame "Var name for key 'plain.test.password' has not been found" 0 $?
+}
+
+. "$test_root_dir/shunit2-2.0.3/src/shell/shunit2"
+[ $__shunit_testsFailed -gt 0 ] && exit 5 || exit 0
+
--- test/shell-util/properties.sh
+++ test/shell-util/properties.sh
@@ -15,17 +15,11 @@ oneTimeSetUp() {
 properties="food=apple
 foo=apple
 animal=lion
-state=idle
-age=11"
-
-properties_2="food=apple
-foo=apple
-animal=lion
 #color=red
 state=idle
 age=11"
 
-properties_3="food=apple
+properties_2="food=apple
  foo   = apple
   animal =cow
  state   = idle   
@@ -35,30 +29,40 @@ age=11"
 
 testFindFirstProperty() {
 	local food="$(echo "$properties" | find_property "food")"
-	assertEquals $? 0
+	assertEquals 0 $?
 	assertEquals apple "$food"
 }
 
 testFindProperty() {
 	local animal="$(echo "$properties" | find_property "animal")"
-	assertEquals $? 0
+	assertEquals 0 $?
 	assertEquals lion "$animal"
 }
 
 testDontFindPropertyInComment() {
-	local color="$(echo "$properties_2" | find_property "#color")"
-	assertFalse "find_property '#color'" "[$? -eq 1]"
+	local color
+	color="$(echo "$properties" | find_property "#color")"
+	assertNotSame "find_property '#color'" 0 $?
 	assertEquals "" "$color"
 	color="$(echo "$properties" | find_property "color")"
-	assertFalse "find_property 'color'" "[$? -eq 1]"
+	assertNotSame "find_property 'color'" 0 $?
 	assertEquals "" "$color"
 }
 
 testFindPropertyWhitSpacesInKeys() {
-	local animal="$(echo "$properties_3" | find_property "animal")"
-	assertEquals $? 0
+	local animal="$(echo "$properties_2" | find_property "animal")"
+	assertEquals 0 $?
 	assertEquals cow "$animal"
 }
+
+testFailToFindProperty() {
+	local animal
+	animal=$(find_property "pirate" "$properties" )
+	local r=$?
+	echo "r=$r"
+	assertNotSame "Property 'pirate' was not found" 0 $r
+}
+
 
 
 
