7c6272a6df5e2976bc7f4b245f3e9eec9b0354f2
Martin Salazard <martin.der.aze@gmail.com>
2017-04-11 23:26:35 +0200

    Use 'shar' for self creating archive

---
+++ build/template/install-post.sh
@@ -0,0 +1,8 @@
+
+chmod +x installer/install.sh || exit 1
+
+./installer/install.sh
+
+echo "*** Fini***"
+
+exit 0
---
+++ build/template/install-pre.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+MDU_INSTALL_TMP_DIR=`mktemp -d` || exit 1
+trap "rm -rf '$MDU_INSTALL_TMP_DIR'" EXIT
+
+echo "m - Working in '$MDU_INSTALL_TMP_DIR'"
+cd "$MDU_INSTALL_TMP_DIR" || exit 1
+
+
+
