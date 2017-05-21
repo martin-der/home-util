efe75fe58bd84161e3f0c05037ecc5661c79ef16
Martin Salazard <martin.der.aze@gmail.com>
2017-05-21 23:48:11 +0200

    Use gradle-shell-plugin

--- test/script/runner.sh
+++ test/script/runner.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-pushd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null
-root_dir="$(pwd -P)/../.."
-popd > /dev/null
-src_root_dir="${root_dir}/src"
-test_root_dir="${root_dir}/test"
+src_root_dir="${MDU_SHELLTEST_PROJECT_DIRECTORY}/../src"
+test_root_dir="${MDU_SHELLTEST_PROJECT_DIRECTORY}"
+
+export PATH="$src_root_dir:$PATH"
 
 
-runTests() {
-	. "${test_root_dir}/shunit2-2.0.3/src/shell/shunit2" || exit 4
-	[ $__shunit_testsFailed -gt 0 ] && exit 5 || exit 0
-}
-
-assertLastCommandFailed() {
-	local last_result
-	last_result=$?
-	[ $# -gt 1 ] && {
-		assertNotSame "$1" "$2" $last_result
-		return $?
-	} || {
-		assertNotSame "Assert Failed" 0 $last_result
-		return $?
-	}
-}
-assertLastCommandSucceeded() {
-	local last_result
-	last_result=$?
-	[ $# -gt 0 ] && {
-		assertEquals "$1" 0 $last_result
-		return $?
-	} || {
-		assertEquals 0 $last_result
-		return $?
-	}
-}
+source "$MDU_SHELLTEST_TESTUNIT_RUNNER_INCLUDE"
